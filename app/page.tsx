61f18aa320164e1869ae26f1da87a1f96a3bfc9f
Suraj Vijay <surajcpp115@gmail.com>
2026-08-31 23:21:45 +0530

    feat: implement AudioPlayer component and integrate audio file handling; remove unused UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,105 +1,32 @@
-import { Button } from "@/components/ui/button"
-import { Card, CardFooter, CardHeader } from "@/components/ui/card"
-import Upload01Icon from "@hugeicons/core-free-icons/Upload01Icon"
-import FileMusicIcon from "@hugeicons/core-free-icons/FileMusicIcon"
-import { HugeiconsIcon } from "@hugeicons/react"
-import { Slider } from "@/components/ui/slider"
-import GoBackward10SecIcon from "@hugeicons/core-free-icons/GoBackward10SecIcon"
-import GoForward10SecIcon from "@hugeicons/core-free-icons/GoForward10SecIcon"
-import PlayIcon from "@hugeicons/core-free-icons/PlayIcon"
-import PauseIcon from "@hugeicons/core-free-icons/PauseIcon"
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
+"use client"
+
+import AudioPlayer from "@/components/custom/audio-player"
+import { useEffect, useState } from "react"
 
 export default function Page() {
+  const [file, setFile] = useState<File>()
+
+  useEffect(() => {
+    const loadFile = async () => {
+      const response = await fetch("/audio/voice_clone_input.mp3")
+      const blob = await response.blob()
+
+      const contentType = response.headers.get("Content-Type") || blob.type
+
+      const fetchedFile = new File([blob], "voice_clone_input.mp3", {
+        type: contentType,
+        lastModified: Date.now(),
+      })
+
+      setFile(fetchedFile)
+    }
+
+    loadFile()
+  }, [])
+
   return (
     <div className="flex min-h-svh items-center justify-center">
-      <Card className="w-[42svw]">
-        <CardHeader className="flex items-center justify-between">
-          <div className="flex items-center gap-2">
-            <HugeiconsIcon icon={FileMusicIcon} color="var(--primary)" />
-            <div>
-              <h4 className="text-md font-semibold">voice_clone_output.mp3</h4>
-              <p className="text-xs text-muted-foreground">
-                Local file · 44.1 kHz
-              </p>
-            </div>
-          </div>
-          <Button size="sm">
-            <HugeiconsIcon icon={Upload01Icon} />
-            Choose File
-          </Button>
-        </CardHeader>
-        <div className="mx-(--card-spacing) flex h-[14svh] items-center justify-center rounded-xl border-2 border-dotted border-border">
-          <p className="text-sm text-muted-foreground">
-            Waveform will be displayed here.
-          </p>
-        </div>
-        <CardFooter className="flex-col gap-(--card-spacing)">
-          <div className="flex w-full flex-col gap-1">
-            <Slider />
-            <div className="flex w-full items-center justify-between text-sm tracking-wide text-muted-foreground">
-              <span>00:00</span>
-              <span>--:--</span>
-            </div>
-          </div>
-          <div className="flex w-full items-center justify-between">
-            <div className="flex items-center gap-1">
-              <Button variant="ghost" size="icon">
-                <HugeiconsIcon
-                  icon={GoBackward10SecIcon}
-                  color="var(--primary)"
-                />
-              </Button>
-              {1 % 1 === 0 ? (
-                <Button
-                  size="icon-lg"
-                  className="size-12 rounded-full [&_svg:not([class*='size-'])]:size-7"
-                >
-                  <HugeiconsIcon
-                    icon={PlayIcon}
-                    fill="var(--primary-foreground)"
-                  />
-                </Button>
-              ) : (
-                <Button
-                  size="icon-lg"
-                  variant="outline"
-                  className="size-12 rounded-full border-2 border-primary [&_svg:not([class*='size-'])]:size-7"
-                >
-                  <HugeiconsIcon
-                    icon={PauseIcon}
-                    fill="var(--primary)"
-                    color="var(--primary)"
-                  />
-                </Button>
-              )}
-              <Button variant="ghost" size="icon">
-                <HugeiconsIcon
-                  icon={GoForward10SecIcon}
-                  color="var(--primary)"
-                />
-              </Button>
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="text-sm text-muted-foreground">Speed</span>
-              <ToggleGroup
-                type="single"
-                size="sm"
-                variant="outline"
-                spacing={0}
-              >
-                {SPEEDS.map(String).map((speed) => (
-                  <ToggleGroupItem key={speed} value={speed}>
-                    {speed}
-                  </ToggleGroupItem>
-                ))}
-              </ToggleGroup>
-            </div>
-          </div>
-        </CardFooter>
-      </Card>
+      {file && <AudioPlayer file={file} setFile={setFile} />}
     </div>
   )
 }
-
-const SPEEDS = [0.5, 1, 1.25, 1.5, 2]
---
+++ components/custom/audio-player.tsx
@@ -0,0 +1,213 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Card, CardFooter, CardHeader } from "@/components/ui/card"
+import ReloadIcon from "@hugeicons/core-free-icons/ReloadIcon"
+import FileMusicIcon from "@hugeicons/core-free-icons/FileMusicIcon"
+import { HugeiconsIcon } from "@hugeicons/react"
+import { Slider } from "@/components/ui/slider"
+import GoBackward10SecIcon from "@hugeicons/core-free-icons/GoBackward10SecIcon"
+import GoForward10SecIcon from "@hugeicons/core-free-icons/GoForward10SecIcon"
+import PlayIcon from "@hugeicons/core-free-icons/PlayIcon"
+import PauseIcon from "@hugeicons/core-free-icons/PauseIcon"
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
+import { useEffect, useRef, useState } from "react"
+import { cn } from "@/lib/utils"
+
+const AudioPlayer: React.FC<AudioPlayerProps> = ({ setFile, file }) => {
+  const fileInputRef = useRef<HTMLInputElement>(null)
+  const audioRef = useRef<HTMLAudioElement | null>(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [totalDuration, setDuration] = useState(0)
+  const [currDuration, setCurrDuration] = useState(0)
+
+  const handleChangeFileClick = () => {
+    fileInputRef.current?.click()
+  }
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || !e.target.files.length) return
+    const uploadedFile = e.target.files[0]
+    setFile(uploadedFile)
+  }
+
+  const handlePlayPause = async () => {
+    const audio = audioRef.current
+    if (!audio) return
+
+    if (audio.paused) {
+      await audio.play()
+    } else {
+      audio.pause()
+    }
+  }
+
+  const handleRewind = async () => {
+    if (!currDuration) return
+
+    const audio = audioRef.current
+    if (!audio) return
+
+    audio.currentTime = audio.currentTime - 10
+  }
+
+  const handleForward = async () => {
+    if (totalDuration - currDuration < 10) return
+
+    const audio = audioRef.current
+    if (!audio) return
+
+    audio.currentTime = audio.currentTime + 10
+  }
+
+  useEffect(() => {
+    if (!file) return
+
+    const url = URL.createObjectURL(file)
+    const audioElement = new Audio(url)
+
+    audioRef.current = audioElement
+
+    const handleOnLoadMetadata = () => setDuration(audioElement.duration)
+    const handleOnTimeUpdate = () => setCurrDuration(audioElement.currentTime)
+
+    const handlePlay = () => setIsPlaying(true)
+    const handlePause = () => setIsPlaying(false)
+    const handleEnded = () => setIsPlaying(false)
+
+    audioElement.addEventListener("loadedmetadata", handleOnLoadMetadata)
+    audioElement.addEventListener("timeupdate", handleOnTimeUpdate)
+
+    audioElement.addEventListener("play", handlePlay)
+    audioElement.addEventListener("pause", handlePause)
+    audioElement.addEventListener("ended", handleEnded)
+
+    return () => {
+      audioElement.removeEventListener("loadedmetadata", handleOnLoadMetadata)
+      audioElement.removeEventListener("timeupdate", handleOnTimeUpdate)
+
+      audioElement.removeEventListener("play", handlePlay)
+      audioElement.removeEventListener("pause", handlePause)
+      audioElement.removeEventListener("ended", handleEnded)
+
+      audioElement.pause()
+      audioElement.src = ""
+      URL.revokeObjectURL(url)
+      audioRef.current = null
+    }
+  }, [file])
+
+  return (
+    <Card className="w-[42svw]">
+      <CardHeader className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <HugeiconsIcon icon={FileMusicIcon} color="var(--primary)" />
+          <div>
+            <h4 className="text-md font-semibold">{file.name}</h4>
+          </div>
+        </div>
+        <>
+          <Button size="sm" onClick={handleChangeFileClick}>
+            <HugeiconsIcon icon={ReloadIcon} />
+            Change File
+          </Button>
+          <input
+            type="file"
+            hidden
+            ref={fileInputRef}
+            onChange={handleFileUpload}
+            accept="audio/*"
+          />
+        </>
+      </CardHeader>
+      <div className="mx-(--card-spacing) flex h-[14svh] items-center justify-center rounded-xl border-2 border-dotted border-border">
+        <p className="text-sm text-muted-foreground">
+          Waveform will be displayed here.
+        </p>
+      </div>
+      <CardFooter className="flex-col gap-(--card-spacing)">
+        <div className="flex w-full flex-col gap-1">
+          <Slider value={[currDuration]} max={totalDuration} />
+          <div className="flex w-full items-center justify-between text-sm tracking-wide text-muted-foreground">
+            <span>{formatSeconds(currDuration)}</span>
+            <span>{formatSeconds(totalDuration)}</span>
+          </div>
+        </div>
+        <div className="flex w-full items-center justify-between">
+          <div className="flex items-center gap-1">
+            <Button
+              variant="ghost"
+              size="icon"
+              className="rounded-full"
+              onClick={handleRewind}
+              disabled={!currDuration}
+            >
+              <HugeiconsIcon
+                icon={GoBackward10SecIcon}
+                color="var(--primary)"
+              />
+            </Button>
+            <Button
+              size="icon-lg"
+              variant={!isPlaying ? "default" : "outline"}
+              className={cn(
+                "size-12 rounded-full [&_svg:not([class*='size-'])]:size-7",
+                isPlaying && "border-2 border-primary"
+              )}
+              onClick={handlePlayPause}
+            >
+              {!isPlaying ? (
+                <HugeiconsIcon
+                  icon={PlayIcon}
+                  fill="var(--primary-foreground)"
+                />
+              ) : (
+                <HugeiconsIcon
+                  icon={PauseIcon}
+                  fill="var(--primary)"
+                  color="var(--primary)"
+                />
+              )}
+            </Button>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="rounded-full"
+              onClick={handleForward}
+              disabled={totalDuration - currDuration < 10}
+            >
+              <HugeiconsIcon icon={GoForward10SecIcon} color="var(--primary)" />
+            </Button>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-sm text-muted-foreground">Speed</span>
+            <ToggleGroup type="single" size="sm" variant="outline" spacing={0}>
+              {SPEEDS.map(String).map((speed) => (
+                <ToggleGroupItem key={speed} value={speed}>
+                  {speed}
+                </ToggleGroupItem>
+              ))}
+            </ToggleGroup>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default AudioPlayer
+
+interface AudioPlayerProps {
+  file: File
+  setFile: React.Dispatch<React.SetStateAction<File | undefined>>
+}
+
+const SPEEDS = [0.5, 1, 1.25, 1.5, 2]
+
+const formatSeconds = (seconds: number): string => {
+  const hours = String(Math.floor(seconds / 3600)).padStart(2, "0")
+  const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(2, "0")
+  const leftOutSeconds = String(Math.floor(seconds % 60)).padStart(2, "0")
+
+  return `${hours[1] !== "0" ? hours : ""}${minutes}:${leftOutSeconds}`
+}
